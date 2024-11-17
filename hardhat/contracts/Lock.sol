790080ef99d481f1517b61021a127775ad5c1a35
Rosen Karamanov <rosenkaramanov@Rosens-MacBook-Pro.local>
2024-11-17 23:47:23 +0200

    Update contract tests

--- hardhat/contracts/Lock.sol
+++ hardhat/contracts/Lock.sol
@@ -1,34 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.27;
-
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+pragma solidity ^0.8.17;
 
 contract Lock {
     uint public unlockTime;
-    address payable public owner;
+    address public owner;
 
-    event Withdrawal(uint amount, uint when);
-
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
+    constructor(uint _unlockTime) {
         unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+        owner = msg.sender;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+    function setUnlockTime(uint _unlockTime) public {
+        require(msg.sender == owner, "Only the owner can set unlock time");
+        unlockTime = _unlockTime;
     }
 }
